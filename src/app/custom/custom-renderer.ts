1702fa3a9239c0300f111c24eeee11b4542e149e
DevAutomationInf <pi.devautomation@infor.com>
2024-11-26 16:21:05 +0100

    custom font on label

--- src/app/custom/custom-renderer.ts
+++ src/app/custom/custom-renderer.ts
@@ -1,5 +1,4 @@
 import BaseRenderer, { BpmnRendererConfig } from 'bpmn-js/lib/draw/BpmnRenderer.js';
-import { getLabelColor } from 'bpmn-js/lib/draw/BpmnRenderUtil';
 import PathMap from 'bpmn-js/lib/draw/PathMap';
 import TextRenderer from 'bpmn-js/lib/draw/TextRenderer';
 import { getLabel } from 'bpmn-js/lib/features/label-editing/LabelUtil';
@@ -8,7 +7,7 @@ import EventBus from 'diagram-js/lib/core/EventBus';
 import Styles from 'diagram-js/lib/draw/Styles';
 import Canvas from 'diagram-js/lib/core/Canvas';
 import TextUtil from 'diagram-js/lib/util/Text';
-import { append as svgAppend} from 'tiny-svg';
+import { append as svgAppend, attr as svgAttr,} from 'tiny-svg';
 
 export class CustomRenderer extends BaseRenderer {
   static override $inject = ['config.bpmnRenderer', 'eventBus', 'styles', 'pathMap', 'canvas', 'textRenderer'];
@@ -33,30 +32,39 @@ export class CustomRenderer extends BaseRenderer {
   }
 
   override canRender(element: Shape): boolean {
-    return (element.type === 'label' && element.businessObject.$type === 'bpmn:Group');
+    return true;
   }
 
   override drawShape(parentGfx: SVGElement, element: Shape): SVGElement {
-      const textUtil = new TextUtil({
-        style: {
-          fontFamily: 'Arial, sans-serif',
-          fontSize: 12,
-          fontWeight: 'normal',
-          lineHeight: 1.2,
-        },
-      });
+    const fontSize = element?.businessObject?.extensionElements?.values.find(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ({ name }: any) => name === 'fontSize',
+    )?.value;
+
+    const textUtil = new TextUtil({
+      style: {
+        fontFamily: 'Arial, sans-serif',
+        fontSize,
+        fontWeight: 'normal',
+        lineHeight: 1.2,
+      },
+    });
+    if(element.type === 'label'){
+
       const label = getLabel(element);
       const box = this.textRenderer.getExternalLabelBounds({ width: 90, height: 30, x: 0, y: 9.8 }, label);
       let text = textUtil.createText(label, {
         align: 'center-top',
         box,
       });
-      if (element.di?.label?.bounds) {
-        const { width, height } = element.di.label.bounds;
-        text = textUtil.createText(getLabel(element), { box: { width, height } });
-      }
       svgAppend(parentGfx, text);
 
       return text;
+    } else {
+
+      element?.di.label?.set('style', {fontSize});
+      console.log('label',element?.di.label)
+      return super.drawShape(parentGfx, element);
+    }
   }
 }
